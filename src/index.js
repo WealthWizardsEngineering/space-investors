4d808bb5ef4c68216d208a002439f8db46c32975
Jody Salt <jody.salt@wealthwizards.com>
2021-12-01 15:09:41 +0000

    refactor: var to const

--- src/index.js
+++ src/index.js
@@ -23,15 +23,15 @@ const config = {
         create() {
             this.add.image(400, 300, 'sky');
         
-            var particles = this.add.particles('red');
+            const particles = this.add.particles('red');
         
-            var emitter = particles.createEmitter({
+            const emitter = particles.createEmitter({
                 speed: 100,
                 scale: { start: 1, end: 0 },
                 blendMode: 'ADD'
             });
         
-            var logo = this.physics.add.image(400, 100, 'logo');
+            const logo = this.physics.add.image(400, 100, 'logo');
         
             logo.setVelocity(100, 200);
             logo.setBounce(1, 1);
