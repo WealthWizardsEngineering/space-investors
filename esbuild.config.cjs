7c02b4c1bd7a7c6b3fba6d4d9cc61c55c37bdbe4
John Obla <jo.github@outlook.com>
2021-12-02 16:37:05 +0000

    feat(esbuild): dont use sourcemap on prod

--- esbuild.config.cjs
+++ esbuild.config.cjs
@@ -1,10 +1,12 @@
 const esbuild = require("esbuild");
 
+const isProd = process.env.NODE_ENV === "production";
+
 esbuild
   .build({
     entryPoints: ["src/index.ts"],
     bundle: true,
-    sourcemap: true,
+    sourcemap: !isProd,
     outfile: "public/app.js",
     target: "esnext",
   })
